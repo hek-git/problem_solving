7cf219725f357bccdd5425b37b24971abfa78caa
hek-git <ghddmsrl1@naver.com>
2023-07-11 00:20:23 +0900

    boj 2615

--- boj/20230710_2615.cpp
+++ boj/20230710_2615.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int map[20][20], ans, w, h;
 
 bool check_win(int a, int b, int color) {
-    
-	int cnt = 0;
+    int cnt = 0;
 
     // 오른쪽 상단 대각선 방향 확인
     for (int i = 0; i < 5; i++) {
@@ -15,17 +14,15 @@ bool check_win(int a, int b, int color) {
         }
     }
     // 6목인지 확인(왼쪽 밑)
-    if (a + 1 < 20 && b - 1 > 0) {
+    if (cnt == 5 && a + 1 < 20 && b - 1 > 0) {
         if (map[a + 1][b - 1] == color) cnt++;
     }
     // 6목인지 확인(오른쪽 위)
-    if (a - 5 > 0 && b + 5 < 20) {
+    if (cnt == 5 && a - 5 > 0 && b + 5 < 20) {
         if (map[a - 5][b + 5] == color) cnt++;
     }
-	cout << endl << cnt << endl;
     // 정확히 다섯개라면 해당 돌과 가장 왼쪽 좌표 반환
-    if (cnt == 5 && (a < 15 && b < 15 && map[a][b] == map[a-5][b+5])) {
-		cout << "1번 경우" << endl;
+    if (cnt == 5 && (a < 16 && b < 15 && map[a][b] == map[a - 4][b + 4])) {
         ans = color;
         w = a;
         h = b;
@@ -40,16 +37,15 @@ bool check_win(int a, int b, int color) {
         }
     }
     // 6목인지 확인(왼쪽)
-    if (b - 1 > 0) {
+    if (cnt == 5 && b - 1 > 0) {
         if (map[a][b - 1] == color) cnt++;
     }
     // 6목인지 확인(오른쪽)
-    if (b + 5 < 20) {
+    if (cnt == 5 && b + 5 < 20) {
         if (map[a][b + 5] == color) cnt++;
     }
     // 정확히 다섯개라면 해당 돌과 가장 왼쪽 좌표 반환
-    if (cnt == 5) {
-		cout << "2번 경우" << endl;
+    if (cnt == 5 && (b < 16 && map[a][b] == map[a][b + 4])) {
         ans = color;
         w = a;
         h = b;
@@ -64,16 +60,15 @@ bool check_win(int a, int b, int color) {
         }
     }
     // 6목인지 확인(왼쪽 위)
-    if (a - 1 > 0 && b - 1 > 0) {
+    if (cnt == 5 && a - 1 > 0 && b - 1 > 0) {
         if (map[a - 1][b - 1] == color) cnt++;
     }
     // 6목인지 확인(오른쪽 밑)
-    if (a + 5 < 20 && b + 5 < 20) {
+    if (cnt == 5 && a + 5 < 20 && b + 5 < 20) {
         if (map[a + 5][b + 5] == color) cnt++;
     }
     // 정확히 다섯개라면 해당 돌과 가장 왼쪽 좌표 반환
-    if (cnt == 5) {
-		cout << "3번 경우" << endl;
+    if (cnt == 5 && (a < 16 && b < 16 && map[a][b] == map[a + 4][b + 4])) {
         ans = color;
         w = a;
         h = b;
@@ -88,22 +83,21 @@ bool check_win(int a, int b, int color) {
         }
     }
     // 6목인지 확인(위쪽)
-    if (a - 1 > 0) {
+    if (cnt == 5 && a - 1 > 0) {
         if (map[a - 1][b] == color) cnt++;
     }
     // 6목인지 확인(아래쪽)
-    if (a + 5 < 20) {
+    if (cnt == 5 && a + 5 < 20) {
         if (map[a + 5][b] == color) cnt++;
     }
     // 정확히 다섯개라면 해당 돌과 가장 왼쪽 좌표 반환
-    if (cnt == 5) {
-		cout << "4번 경우" << endl;
+    if (cnt == 5 && (a < 16 && map[a][b] == map[a + 4][b])) {
         ans = color;
         w = a;
         h = b;
         return true;
     }
-	return false;
+    return false;
 }
 
 int main() {
@@ -114,17 +108,18 @@ int main() {
             cin >> num;
             map[i][j] = num;
         }
-	}
+    }
 
     for (int i = 1; i < 20; i++) {
         for (int j = 1; j < 20; j++) {
             if (map[j][i]) {
-                if (check_win(j, i, map[j][i])){
-                    cout << ans << endl << w << " " << h;
-					return 0; 
-				}
+                if (check_win(j, i, map[j][i])) {
+                    cout << ans << endl
+                         << w << " " << h;
+                    return 0;
+                }
             }
         }
     }
-	cout << 0;
+    cout << 0;
 }
