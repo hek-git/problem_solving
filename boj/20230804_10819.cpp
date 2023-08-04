7a4e23706696ab8bf7b4ef1d8409a2217c1b509a
hek-git <ghddmsrl1@naver.com>
2023-08-04 23:16:26 +0900

    boj 10819

---
+++ boj/20230804_10819.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int N, num, ans;
+vector<int> v;
+
+int f(vector<int> v) {
+    int n;
+    for (int i = 0; i < v.size() - 1; i++) {
+        n += abs(v[i] - v[i+1]);
+    }
+    return n;
+}
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> num;
+        v.push_back(num);
+    }
+
+    sort(v.begin(), v.end());
+
+    do {
+        ans = max(ans, f(v));
+    } while (next_permutation(v.begin(), v.end()));
+
+    cout << ans;
+}
