d2c57c122736554a1d08bdbc2a4a11384926d3be
heg-git <ghddmsrl1@naver.com>
2023-09-15 23:22:59 +0900

    BOJ 15657

---
+++ boj/20230915_15657.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M, num, check[10001];
+vector<int> number, ans;
+
+void backTracking(int cnt, int idx) {
+    if (cnt == M) {
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = idx; i < N; i++) {
+        ans.push_back(number[i]);
+        backTracking(cnt + 1, i);
+        ans.pop_back();
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        cin >> num;
+        number.push_back(num);
+    }
+    sort(number.begin(), number.end());
+    backTracking(0, 0);
+}
